209abfcd33fb44be4b878ed6461b059e828a5cf5
Tru-Dev <29785998+Tru-Dev@users.noreply.github.com>
2021-05-13 13:29:40 -0700

    New Label class

---
+++ MatchingGame/MatchingLabel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MatchingGame
+{
+    public partial class MatchingLabel : Label
+    {
+        
+
+        public MatchingLabel()
+        {
+            InitializeComponent();
+
+            TextAlign = ContentAlignment.MiddleCenter;
+            Dock = DockStyle.Fill;
+            AutoSize = false;
+            Font = new Font("Segoe UI Light", 32f, FontStyle.Regular);
+
+            Click += MatchingClick;
+        }
+
+        private void MatchingClick(object sender, EventArgs e)
+        {
+            MatchingLabel clicked = sender as MatchingLabel;
+            if (clicked != null)
+            {
+                if (clicked.ForeColor != Color.Black) {
+                    clicked.ForeColor = Color.Black;
+                }
+            }
+        }
+
+        protected override void OnPaint(PaintEventArgs pe)
+        {
+            base.OnPaint(pe);
+        }
+    }
+}
