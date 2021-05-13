93e5cca651a435cf30f84caebece33a420e19760
Tru-Dev <29785998+Tru-Dev@users.noreply.github.com>
2021-05-13 13:30:34 -0700

    Added matching functionality to Form

--- MatchingGame/Form1.cs
+++ MatchingGame/Form1.cs
@@ -12,9 +12,38 @@ namespace MatchingGame
 {
     public partial class Form1 : Form
     {
+        Random random = new Random();
+
+        List<string> matchingItems = new List<string>()
+        {
+            "2", "1 + 1", "3", "1 + 2", "4", "2 + 2", "5", "2 + 3",
+            "6", "3 + 3", "7", "3 + 4", "8", "4 + 4", "9", "4 + 5",
+        };
+
+        MatchingLabel curr1st = null;
+        MatchingLabel curr2nd = null;
+
+
+        private void AssignNumbersToSquares()
+        {
+            int amtIters = matchingItems.Count;
+            for (int i = 0; i < amtIters; i++)
+            {
+                int randomNumber = random.Next(matchingItems.Count);
+                tableLayoutPanel1.Controls.Add(new MatchingLabel
+                {
+                    Text = matchingItems[randomNumber],
+                    ForeColor = tableLayoutPanel1.BackColor
+                });
+                matchingItems.RemoveAt(randomNumber);
+            }
+        }
+
         public Form1()
         {
             InitializeComponent();
+
+            AssignNumbersToSquares();
         }
 
     }
